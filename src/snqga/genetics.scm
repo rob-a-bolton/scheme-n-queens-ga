a06fe0e7184b1eddbdc67864621101ab291240c4
Rob Bolton <rab26@aber.ac.uk>
2016-12-05 18:27:13 +0000

    Added mutation functions

--- src/snqga/genetics.scm
+++ src/snqga/genetics.scm
@@ -60,6 +60,23 @@
 		     (cdr remaining)
 		     (+ total (caar remaining)))))))
 
+(define (mutate-gene gene n)
+  "Mutates either the X or Y of a gene."
+  (if (< (random:uniform) 0.5)
+      (cons (random n) (cdr gene))
+      (cons (car gene) (random n))))
+
+(define (mutate! chromosome)
+  "Mutates a chromosome."
+  (let* ((index (random (length chromosome)))
+	 (gene (list-ref chromosome index))
+	 (new-gene (mutate-gene gene (length chromosome))))
+    (if (member new-gene chromosome)
+	(mutate chromosome)
+	(begin
+	  (list-set! chromosome index new-gene)
+	  chromosome))))
+
 (define (chromosome->board chromosome)
   "Creates a printable string representation of a chromosome as a chess board."
   (let* ((n (length chromosome))
