f0a2527f7863852398e8f09ca57861154f61b564
Rob Bolton <rab26@aber.ac.uk>
2016-12-07 12:13:12 +0000

    Fixed infinite loop to do with parent ratio and float results

--- src/snqga/genetics.scm
+++ src/snqga/genetics.scm
@@ -13,7 +13,7 @@
 			parent-ratio
 			mutation-chance)
   "Runs over a single generation. Performs selection, crossover, and mutation."
-  (let* ((parents (selection-function generation (/ (length generation) parent-ratio)))
+  (let* ((parents (selection-function generation (floor (/ (length generation) parent-ratio))))
 	 (crossed-over (map-crossover parents crossover-function))
 	 (combined (append crossed-over generation)))
     (selection-function
